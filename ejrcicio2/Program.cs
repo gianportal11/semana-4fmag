d6b35e9ab10786e8461e1fa3502fb509c6227c31
Anderson Jair <anderson@upn.pe>
2025-04-16 17:24:22 -0500

    ejercicio2

---
+++ ejrcicio2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejrcicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double n1, n2, n3, n4;
+            double promedio;
+            Console.WriteLine("ingrese n1 (10%): ");
+            n1= float.Parse(Console.ReadLine());
+            Console.WriteLine("ingrese n2 (20%): ");
+            n2 = float.Parse(Console.ReadLine());
+            Console.WriteLine("ingrese n3 (30): ");
+            n3 =float.Parse(Console.ReadLine());
+            Console.WriteLine("ingrese n4 (40%): ");
+            n4 = float.Parse(Console.ReadLine());
+            promedio = (n1*0.10) + (n2*0.20) + (n3*0.30) + (n4*0.40);
+            Console.WriteLine("peomedio: " + promedio);
+            
+            if (promedio >= 11.5)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("APROBADO");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("DESAPROBADO");
+
+            }
+
+            Console.ForegroundColor = ConsoleColor.Gray;
+
+        }
+    }
+}
